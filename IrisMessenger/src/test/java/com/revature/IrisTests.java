f6891009cbd72173790307463e3e41f721ff6c68
You Lin <youlin404@gmail.com>
2022-05-17 15:38:07 -0400

    added junit test and folder

---
+++ IrisMessenger/src/test/java/com/revature/IrisTests.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IrisTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
