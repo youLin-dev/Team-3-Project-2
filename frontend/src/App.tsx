683ddd2536b1ee6c8db695a0d0867499bf9599fa
Alex Proctor <taproctor00@gmail.com>
2022-05-19 16:54:18 -0400

    merging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-<<<<<<< HEAD
-import {LoginPage} from './Components/Login/login2';
-import ProfilePage from './Components/Profile/Profile';
-=======
 import { LoginPage } from './Components/Login/login2';
-import { ProfilePage } from './Components/Profile/Profile';
->>>>>>> f46cfc334a24332ee37472b9c07cf7fe23dcbaf5
-
+import {ProfilePage} from './Components/Profile/Profile';
 function App() {
-  //+919782795905 (India) Whatdapp
+  //+919782795905 (India) Whatdap
   return (
     <div> 
       <Router>
--- frontend/src/Components/Login/login2.tsx
+++ frontend/src/Components/Login/login2.tsx
@@ -7,15 +7,11 @@ import { loginUser } from "../../actions/UserActions"
 
 import "./Login2.css"
 import RegisterPage from "./Register"
-export const LoginPage: React.FC<any>= ()=>{
+//export const LoginPage: React.FC<any>= ()=>{
 
-<<<<<<< HEAD
-    /*Missing code from login.tsx */
-=======
 export const LoginPage: React.FC<any> = (props:any)=>{
 
 /*Missing code from login.tsx */
->>>>>>> f46cfc334a24332ee37472b9c07cf7fe23dcbaf5
     //this is how we access the state in the store. The data in the universal data file.
     const appState = useSelector<any, any>((state: any) => state);
     //we need this object to actually dispatch data to our store
@@ -113,12 +109,8 @@ export const LoginPage: React.FC<any> = (props:any)=>{
   
   
   </div>
-<<<<<<< HEAD
-*/
-=======
 
 </div>
 
 )
-}
->>>>>>> f46cfc334a24332ee37472b9c07cf7fe23dcbaf5
+}*/
